0fcb4b9f8ed05d181e5eff5a180452f3d39b2bde
riro9510 <riro9510@gmail.com>
2024-10-12 20:01:13 -0600

    .

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -15,8 +15,8 @@ const hamburger = document.getElementById('hamburger');
 const nav = document.querySelector('header nav:first-of-type');
 
 hamburger.addEventListener('click', () => {
-    nav.classList.toggle('open'); // Alterna la clase 'open' en el nav
-    hamburger.textContent = nav.classList.contains('open') ? '✖' : '☰'; // Cambiar icono
+    nav.classList.toggle('open'); 
+    hamburger.textContent = nav.classList.contains('open') ? '✖' : '☰'; 
 });
 
 const temples = [
